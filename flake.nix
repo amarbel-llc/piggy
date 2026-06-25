77dd5378942916b3edc3eb72270f6242771bbee6
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-25 11:50:20 -0400

    bump nixpkgs-master: d233902 → 567a49d

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # `amarbel-llc/bats` below — not from `pkgs.testers.batsLane`, which
     # the bats flake no longer ships through this overlay.
     igloo.url = "github:amarbel-llc/igloo";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
 
     bats = {
